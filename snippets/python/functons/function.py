21ea8470850b32d0534554d19c537f4737001812
polavarapuumesh <polavarapuumesh9@gmail.com>
2024-07-10 11:11:15 +0530

    files will be in a structured ordernow

---
+++ snippets/python/functons/function.py
@@ -0,0 +1,29 @@
+#functions_practise1
+def msg(name):
+    print("hello,"+name)
+msg("umesh")
+msg("ganesh")
+
+
+#
+def fun(grammer):
+    print("let me,"+grammer)
+fun("check")
+fun("verify")
+
+#add 2 fun
+def add(a,b,c):
+    return a+b+c
+result=add(22,34,-11)
+print(result)
+
+#return_value
+def num(c,d):
+    mul=2*8
+    print(mul)
+
+#
+def math(y,t):
+    sub=y-t
+    return sub
+
