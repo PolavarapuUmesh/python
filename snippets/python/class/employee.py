ad66a9c9958b2ab1e8789aa9024dee69b5f19973
polavarapuumesh <polavarapuumesh9@gmail.com>
2024-07-28 22:02:38 +0530

    modified the class snippet

--- snippets/python/class/employee.py
+++ snippets/python/class/employee.py
@@ -1,24 +1,14 @@
-class employee:       #employee class uses pass to create an empty class definition
-    pass              #pass is a statemnet that gives you to skip for now and use it further use
-emp1=employee()       #emp1 is a instance variable 
-emp2=employee()       #emp2 is a instance variable
-emp3=employee()       #emp3 is a instance variable 
-print(emp1,emp2,emp3) 
+class employee:            #employee class uses pass to create an empty class definition
+    def __init__(self,first,last,email,pay):
+        self.first=first
+        self.last=last
+        self.email=email
+        self.pay=pay       #pass is a statemnet that gives you to skip for now and use it further use
+emp1=employee("polavarapu","umesh","umeshpolavarapu71@gmail.com",20000)            #emp1 is a instance variable 
+emp2=employee("polavarapu","suresh","sureshpolavarapu71@gmail.com",70000)            #emp2 is a instance variable
+emp3=employee("polavarapu","rajaneesh","rajaneeshpolavrapu@gmail.com",400000)            #emp3 is a instance variable 
 
-emp1.first="polavarapu" #assigned attribute to the emp1 instance variable
-emp1.last="umesh"
-emp1.email="umeshpolavarapu71@gmail.com"
-emp1.pay=20,000
-
-emp2.first="polavarapu"
-emp2.last="suresh"
-emp2.email="sureshpolavarapu71@gmail.com"
-emp2.pay=70,000
-
-emp3.first="polavarapu"
-emp3.last="rajanessh"
-emp3.email="rajaneeshpolavarapu@gmail.com"
-emp3.pay=4,00,000
-
-print(emp1.email,emp2.email,emp3.email)
-
+print("employees first name:",[emp1.first,emp2.first,emp3.first])
+print("employees last name:",[emp1.last,emp2.last,emp3.last])
+print("employees email:",[emp1.email,emp2.email,emp3.email])
+print("employees pay:",[emp1.pay,emp2.pay,emp3.pay])
